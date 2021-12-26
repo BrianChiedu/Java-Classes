95c6dce21aad27e423f91436d9a9814b00c8b498
BrianChiedu <81457860+BrianChiedu@users.noreply.github.com>
2021-12-26 14:38:34 +0000

    Question 1 on 2D arrays and methods

---
+++ question1main.java
@@ -0,0 +1,26 @@
+
+public class question1main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+				char[][] chararray = new char [3][6];
+		//		char[][] chararray1 = {  {G,B,A,C,A,B},
+		//								{K,D,X,F,O,B},
+		//								{Z,H,S,A,D,C} };
+		//		
+				char character1 = 'B';
+				char character2 = 'H';
+		
+			}
+			
+			public static int replacementmethod(char[][] chararray, char a,  char n) {
+		
+				
+				
+			}
+		}
+	
+
+
