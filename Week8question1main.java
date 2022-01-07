55875119517051747ab6492d74de817350b6f6ea
BrianChiedu <81457860+BrianChiedu@users.noreply.github.com>
2022-01-07 21:54:41 +0000

    Update Week8question1main.java

--- Week8question1main.java
+++ Week8question1main.java
@@ -5,22 +5,31 @@ public class question1main {
 		// TODO Auto-generated method stub
 		
 
-				char[][] chararray = new char [3][6];
+				char[][] charArray = new char [3][6];
 		//		char[][] chararray1 = {  {G,B,A,C,A,B},
 		//								{K,D,X,F,O,B},
 		//								{Z,H,S,A,D,C} };
 		//		
 				char character1 = 'B';
 				char character2 = 'H';
+				
+				int replacements = replacementmethod(charArray,character1, character2 );
+				System.out.println("Number of replaced letters are " + replacements);
 		
 			}
-			
-			public static int replacementmethod(char[][] chararray, char a,  char n) {
-		
+	
+//		Function that replaces the letter	
+	public static int replacementmethod(char[][] charArray, char a,  char n) {
+				
+				int replacedAmount = 0;
 				
 				
+				
+				return replacedAmount;
 			}
 		}
 	
+// Function that prints the 2D array
+
 
 
