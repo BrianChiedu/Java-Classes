1b74daa9dd9bdb2c9bcfc62f5960b5e19c0ba5d1
Opemipo Bolaji <bolajipemipo@gmail.com>
2022-01-08 13:38:04 +0100

    QuestionOne

--- Week8question1main.java
+++ Week8question1main.java
@@ -1,35 +1,51 @@
+class QuestionOne {
 
-public class question1main {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+        char[][] charArray = {
+                {'G', 'B', 'A', 'C', 'A', 'B'},
+                {'K', 'D', 'X', 'F', 'O', 'B'},
+                {'Z', 'H', 'S', 'A', 'D', 'C'}
+        };
 
-				char[][] charArray = new char [3][6];
-		//		char[][] chararray1 = {  {G,B,A,C,A,B},
-		//								{K,D,X,F,O,B},
-		//								{Z,H,S,A,D,C} };
-		//		
-				char character1 = 'B';
-				char character2 = 'H';
-				
-				int replacements = replacementmethod(charArray,character1, character2 );
-				System.out.println("Number of replaced letters are " + replacements);
-		
-			}
-	
-//		Function that replaces the letter	
-	public static int replacementmethod(char[][] charArray, char a,  char n) {
-				
-				int replacedAmount = 0;
-				
-				
-				
-				return replacedAmount;
-			}
-		}
-	
-// Function that prints the 2D array
+        char character1 = 'B';
+        char character2 = 'H';
+
+        displayArray(charArray);
+
+        int replacements = replacementmethod(charArray, character1, character2);
+
+        displayArray(charArray);
+
+        System.out.println("Number of replaced letters are " + String.valueOf(replacements));
+    }
+
+    public static int replacementmethod(char[][] charArray, char character1, char character2) {
+        int replacedAmount = 0;
+
+        for (int i = 0; i < charArray.length; i++) {
+            for (int j = 0; j < charArray[i].length; j++) {
+
+                if (charArray[i][j] == character1) {
+                    charArray[i][j] = character2;
+
+                    replacedAmount++;
+                }
+            }
+        }
+
+        return replacedAmount;
+    }
+
+    public static void displayArray(char[][] charArray) {
+        for (int i = 0; i < charArray.length; i++) {
+            System.out.println(charArray[i]);
+        }
+
+        System.out.println("---------------------------------");
+    }
+}
+
 
 
 
